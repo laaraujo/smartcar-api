e9b34f85db8e5238276fb94cff1d5ebc675671b2
laaraujo <lautaroarielaraujo@gmail.com>
2024-11-10 03:06:11 -0300

    omit app logs during tests

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,9 @@ const swaggerOptions = {
 
 const app = express();
 
-app.use(morgan("tiny"));
+app.use(
+  morgan("tiny", { skip: (_req, _res) => process.env.NODE_ENV === "test" })
+);
 
 app.use(express.json());
 
